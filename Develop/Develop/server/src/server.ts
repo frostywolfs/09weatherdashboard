85509e377b99824a8a2e45ee4fc462daa907e44b
Christopher Frost <frostyjumper25@gmail.com>
2025-02-27 20:53:29 -0600

    added-.gitignore

--- Develop/Develop/server/src/server.ts
+++ Develop/Develop/server/src/server.ts
@@ -4,17 +4,24 @@ dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
+import { json } from 'node:stream/consumers';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+process.env.API_BASE_URL
+process.env.API_KEY
 
-// TODO: Serve static files of entire client dist folder
+// ✅TODO: Serve static files of entire client dist folder
+app.use(express.static('../../../client'));
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
+// ✅TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
 
-// TODO: Implement middleware to connect the routes
-app.use(routes);
+app.use(express.urlencoded( { extended: true }));
+
+// ✅TODO: Implement middleware to connect the routes
+app.use('/', routes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
